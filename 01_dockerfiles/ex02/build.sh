6418fe64726f38c3e9d82aa08b02549022712259
kahila <adonis7121@gmail.com>
2020-11-22 20:35:36 +0200

    ex02 done

--- 01_dockerfiles/ex02/build.sh
+++ 01_dockerfiles/ex02/build.sh
@@ -8,3 +8,4 @@ docker build -t ex02 .
 docker run -it --rm -p 3000:3000 ex02
 
 #app used is takeb from https://github.com/JetBrains/sample_rails_app
+# git clone --recurse-submodules repo-link should be used to clone
