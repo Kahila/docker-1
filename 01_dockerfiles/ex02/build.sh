3e96217f3127663e42e3a6a53b42a3d4a10c3855
kahila <adonis7121@gmail.com>
2020-11-22 20:15:24 +0200

    ex02 done

---
+++ 01_dockerfiles/ex02/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#building the rails image
+cd ft-rails
+docker build -t ft-rails:on-build .
+cd ..
+docker build -t ex02 .
+docker run -it --rm -p 3000:3000 ex02
+
+#app used is takeb from https://github.com/JetBrains/sample_rails_app
